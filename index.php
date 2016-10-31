d1f20f0f728028e1cc0d3c3c26e39cf17dd6f446
1308830 <dennis1_obrien@hotmail.com>
2016-10-31 15:21:36 +0000

    html setup

---
+++ index.php
@@ -0,0 +1,20 @@
+
+/**
+ * Created by PhpStorm.
+ * User: 1308830
+ * Date: 31/10/2016
+ * Time: 14:56
+ */
+
+<!DOCTYPE html>
+<html>
+    <head>
+        </head>
+    <body>
+        <p>
+            <?php
+            echo "hmtl output"
+            ?>
+        </p>
+    </body>
+</html>
